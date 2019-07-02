6b30462a03578b185b27ed27762d8b47d2128519
Reinhard <lijianbo@newgame.com>
2019-07-02 20:27:47 +0800

    solve 5.2 SwapBits

--- epi_judge_java_solutions/epi/SwapBits.java
+++ epi_judge_java_solutions/epi/SwapBits.java
@@ -3,27 +3,64 @@ package epi;
 import epi.test_framework.EpiTest;
 import epi.test_framework.GenericTest;
 
+/**
+ * 5.2 SWAP BITS
+ * <p>
+ * A 64-bit integer can be viewed as an array of 64 bits, with the bit at index 0 corre¬
+ * sponding to the least significant bit (LSB), and the bit at index 63 corresponding to
+ * the most significant bit (MSB). Implement code that takes as input a 64-bit integer
+ * and swaps the bits at indices i and j.
+ */
 public class SwapBits {
-
-  @EpiTest(testDataFile = "swap_bits.tsv")
-  public static long swapBits(long x, int i, int j) {
-
-    // Extract the i-th and j-th bits, and see if they differ.
-    if (((x >>> i) & 1) != ((x >>> j) & 1)) {
-      // i-th and j-th bits differ. We will swap them by flipping their values.
-      // Select the bits to flip with bitMask. Since x^1 = 0 when x = 1 and 1
-      // when x = 0, we can perform the flip XOR.
-      long bitMask = (1L << i) | (1L << j);
-      x ^= bitMask;
+    @EpiTest(testDataFile = "swap_bits.tsv")
+    public static long swapBits(long x, int i, int j) {
+        return solTow(x, i, j);
     }
-    return x;
-  }
 
-  public static void main(String[] args) {
-    System.exit(
-        GenericTest
-            .runFromAnnotations(args, "SwapBits.java",
-                                new Object() {}.getClass().getEnclosingClass())
-            .ordinal());
-  }
+    /**
+     * 读取位 i 和位 j，不相等再进行交换，一次修改一位
+     * <p>
+     * 时间复杂度：O(1)
+     */
+    private static long solOne(long x, int i, int j) {
+        int bitI = (int) ((x >>> i) & 1L);
+        int bitJ = (int) ((x >>> j) & 1L);
+        if (bitI == bitJ) {
+            return x;
+        }
+
+        if (bitI == 1) {
+            x |= 1L << j;
+        } else {
+            x &= ~(1L << j);
+        }
+        if (bitJ == 1) {
+            x |= 1L << i;
+        } else {
+            x &= ~(1l << i);
+        }
+        return x;
+    }
+
+    /**
+     * 读取位 i 和位 j，不相等再进行交换，利用异或的特性，使用位掩码一次完成全部修改
+     * <p>
+     * 时间复杂度：O(1)
+     */
+    private static long solTow(long x, int i, int j) {
+        if (((x >>> i) & 1L) != ((x >>> j) & 1L)) {
+            long bitMask = (1L << i) | (1L << j);
+            x ^= bitMask;
+        }
+        return x;
+    }
+
+    public static void main(String[] args) {
+        System.exit(
+                GenericTest
+                        .runFromAnnotations(args, "SwapBits.java",
+                                new Object() {
+                                }.getClass().getEnclosingClass())
+                        .ordinal());
+    }
 }
