5257ea5860ff48ebd2967e84773ab9f45a31832c
李剑波 <jianbo.li01@ucarinc.com>
2019-11-11 15:57:41 +0800

    solve 7.3 SpreadsheetEncoding

--- epi_judge_java_solutions/epi/SpreadsheetEncoding.java
+++ epi_judge_java_solutions/epi/SpreadsheetEncoding.java
@@ -3,19 +3,48 @@ package epi;
 import epi.test_framework.EpiTest;
 import epi.test_framework.GenericTest;
 
+/**
+ * 7.3 COMPUTE THE SPREADSHEET COLUMN ENCODING
+ * <p>
+ * Spreadsheets often use an alphabetical encoding of the successive columns.
+ * Specifically, columns are identified by "A", "B", "C", ..., "X", "Y", "Z", "AA",
+ * "AB", ..., "ZZ", "AAA", "AAB", ....
+ * <p>
+ * Implement a function that converts a spreadsheet column id to the corresponding
+ * integer, with "A" corresponding to 1. For example, you should return 4 for "D", 27
+ * for "AA", 702 for "ZZ", etc. How would you test your code?
+ * <p>
+ * Hint: There are 26 characters in ["A", "Z"], and each can be mapped to an integer.
+ */
 public class SpreadsheetEncoding {
-  @EpiTest(testDataFile = "spreadsheet_encoding.tsv")
+    @EpiTest(testDataFile = "spreadsheet_encoding.tsv")
 
-  public static int ssDecodeColID(final String col) {
+    public static int ssDecodeColID(final String col) {
+        return solOne(col);
+    }
 
-    return col.chars().reduce(0, (result, c) -> result * 26 + c - 'A' + 1);
-  }
+    /**
+     * 思路一：等价于 26 进制转 10 进制，'A' 对应 1，以此类推
+     * <p>
+     * 时间复杂度：O(n)
+     * <p>
+     * 空间复杂度：O(1)
+     */
+    private static int solOne(final String col) {
+        int result = 0;
+        for (int i = 0; i < col.length(); i++) {
+            char c = col.charAt(i);
+            result = result * 26 + c - 'A' + 1;
+        }
+        return result;
+    }
 
-  public static void main(String[] args) {
-    System.exit(
-        GenericTest
-            .runFromAnnotations(args, "SpreadsheetEncoding.java",
-                                new Object() {}.getClass().getEnclosingClass())
-            .ordinal());
-  }
+    public static void main(String[] args) {
+        System.exit(
+                GenericTest
+                        .runFromAnnotations(args, "SpreadsheetEncoding.java",
+                                new Object() {
+                                }.getClass().getEnclosingClass())
+                        .ordinal());
+    }
 }
