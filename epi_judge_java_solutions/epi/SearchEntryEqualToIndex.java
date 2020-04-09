2ed080b374f01d1a2ff6c72f779b83d36a55e925
李剑波 <jianbo.li01@ucarinc.com>
2020-04-09 10:25:40 +0800

    solve 12.2 SearchEntryEqualToIndex

--- epi_judge_java_solutions/epi/SearchEntryEqualToIndex.java
+++ epi_judge_java_solutions/epi/SearchEntryEqualToIndex.java
@@ -7,50 +7,93 @@ import epi.test_framework.TimedExecutor;
 
 import java.util.List;
 
+/**
+ * 12.2 SEARCH A SORTED ARRAY FOR ENTRY EQUAL TO ITS INDEX
+ * <p>
+ * Design an efficient algorithm that takes a sorted array of distinct integers,
+ * and returns an index i such that the element at index i equals i. For example,
+ * when the input is (-2,0,2,3,6,7,9) your algorithm should return 2 or 3.
+ * <p>
+ * Hint: Reduce this problem to ordinary binary search.
+ */
 public class SearchEntryEqualToIndex {
 
-  public static int searchEntryEqualToItsIndex(List<Integer> A) {
-
-    int left = 0, right = A.size() - 1;
-    while (left <= right) {
-      int mid = left + ((right - left) / 2);
-      int difference = A.get(mid) - mid;
-      // A.get(mid) == mid if and only if difference == 0.
-      if (difference == 0) {
-        return mid;
-      } else if (difference > 0) {
-        right = mid - 1;
-      } else { // difference < 0.
-        left = mid + 1;
-      }
+    public static int searchEntryEqualToItsIndex(List<Integer> A) {
+        return solTwo(A);
     }
-    return -1;
-  }
 
-  @EpiTest(testDataFile = "search_entry_equal_to_index.tsv")
-  public static void searchEntryEqualToItsIndexWrapper(TimedExecutor executor,
-                                                       List<Integer> A)
-      throws Exception {
-    int result = executor.run(() -> searchEntryEqualToItsIndex(A));
-
-    if (result != -1) {
-      if (A.get(result) != result) {
-        throw new TestFailure("Entry does not equal to its index");
-      }
-    } else {
-      for (int i = 0; i < A.size(); ++i) {
-        if (A.get(i) == i) {
-          throw new TestFailure("There are entries which equal to its index");
+    /**
+     * 思路二：观察法，有序、唯一的数组具有如下特性：
+     * <p>
+     * 1、如果 A[i] < i，那么 i 左侧的元素都不满足
+     * <p>
+     * 2、如果 A[i] > i，那么 i 右侧的元素都不满足
+     * <p>
+     * 也可将问题做一层转换：转换数组，新数组的元素 = A[i] - i，然后查找值为 0 的元素
+     * <p>
+     * 时间复杂度：O(log(n))
+     * <p>
+     * 空间复杂度：O(1)
+     */
+    private static int solTwo(List<Integer> A) {
+        int lo = 0, hi = A.size() - 1;
+        while (lo <= hi) {
+            int mid = (lo + hi) >>> 1;
+            int difference = A.get(mid) - mid;
+            if (difference < 0) {
+                lo = mid + 1;
+            } else if (difference > 0) {
+                hi = mid - 1;
+            }
+            // A.get(mid) == mid if and only if difference == 0.
+            else {
+                return mid;
+            }
         }
-      }
+        return -1;
     }
-  }
 
-  public static void main(String[] args) {
-    System.exit(
-        GenericTest
-            .runFromAnnotations(args, "SearchEntryEqualToIndex.java",
-                                new Object() {}.getClass().getEnclosingClass())
-            .ordinal());
-  }
+    /**
+     * 思路一：遍历数组，找到第一个满足条件的元素
+     * <p>
+     * 时间复杂度：O(n)
+     * <p>
+     * 空间复杂度：O(1)
+     */
+    private static int solOne(List<Integer> A) {
+        for (int i = 0; i < A.size(); i++) {
+            if (A.get(i) == i) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    @EpiTest(testDataFile = "search_entry_equal_to_index.tsv")
+    public static void searchEntryEqualToItsIndexWrapper(TimedExecutor executor,
+                                                         List<Integer> A)
+            throws Exception {
+        int result = executor.run(() -> searchEntryEqualToItsIndex(A));
+
+        if (result != -1) {
+            if (A.get(result) != result) {
+                throw new TestFailure("Entry does not equal to its index");
+            }
+        } else {
+            for (int i = 0; i < A.size(); ++i) {
+                if (A.get(i) == i) {
+                    throw new TestFailure("There are entries which equal to its index");
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        System.exit(
+                GenericTest
+                        .runFromAnnotations(args, "SearchEntryEqualToIndex.java",
+                                new Object() {
+                                }.getClass().getEnclosingClass())
+                        .ordinal());
+    }
 }
